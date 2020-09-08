6ec7a2b1024cd56e655a1ff16ee934245de4b14d
Bombohog <70882697+Bombohog@users.noreply.github.com>
2020-09-08 09:43:34 +0200

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,20 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
+/*
+ * "Projektet" handler om at få udskrevet eller printe en besked.
+ *
+ * Denne kode er programmeret af:
+ * Lasse Jørgensen Kongsdal
+ *
+ * Programmet printer "Hello World!" ud lige så snart den bliver kørt/startet.
+ */
+
+public class Hello {
+        // psvm
+        public static void main(String[] args) {
+            //sout
+            System.out.println("Hello World!");
+        }
+}
